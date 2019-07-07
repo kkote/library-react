0807fa22d4b0729b1cb8ad0ec0004b85ca661feb
Katherine Kote <kaygray521@gmail.com>
2019-07-07 18:39:15 -0500

    Updates

---
+++ precache-manifest.a68d90ffea0110f0058be41d21cf10fe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d76d87ca87a6ba96176ea4567a674ddc",
+    "url": "/library-react/index.html"
+  },
+  {
+    "revision": "890a9edecf1cbe96f164",
+    "url": "/library-react/static/css/main.f8f5f783.chunk.css"
+  },
+  {
+    "revision": "ca5350e0ae8087e6cf5a",
+    "url": "/library-react/static/js/2.b0b0f7ae.chunk.js"
+  },
+  {
+    "revision": "890a9edecf1cbe96f164",
+    "url": "/library-react/static/js/main.587bfd2f.chunk.js"
+  },
+  {
+    "revision": "9aaa8344e455ab72a0f3",
+    "url": "/library-react/static/js/runtime~main.5247af99.js"
+  },
+  {
+    "revision": "d37788b66ee49637262857d938de092c",
+    "url": "/library-react/static/media/bannerbook1.d37788b6.jpg"
+  }
+]);
